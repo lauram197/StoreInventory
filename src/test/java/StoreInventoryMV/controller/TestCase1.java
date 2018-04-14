f8d2d52edfc00b6eed741d3a828ecf4459d96416
Laura <laura.m197@yahoo.com>
2018-04-14 18:07:50 +0300

    lab2v2

--- src/test/java/StoreInventoryMV/controller/TestCase1.java
+++ src/test/java/StoreInventoryMV/controller/TestCase1.java
@@ -1,7 +1,57 @@
 package StoreInventoryMV.controller;
 
+import controller.StoreController;
+import junit.framework.TestCase;
+import model.Product;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * Created by Laura on 29.03.2018.
  */
-public class TestCase1 {
+public class TestCase1 extends TestCase{
+
+    private StoreController ctrl;
+    private String emptyMessage = "Id, Name or address cannot be empty!";
+
+    @Before
+    public void setUp() throws Exception{
+        super.setUp();
+
+        ctrl = new StoreController();
+        //add a client with a normal name
+        //Product p1=new Product(1,"a","a",1);
+        //ctrl.addProduct(p1);
+        //add a client with a name with spaces
+        //Product p2=new Product(2,"b","b",1);
+        //ctrl.addProduct(p2);
+
+    }
+
+    @Test
+    public void testAddProduct1() throws Exception {
+
+        //name is empty
+        Product p1=new Product(1,"a","a",1);
+        ctrl.addProduct(p1);
+        List<Product> productList = ctrl.stockSituation();
+        System.out.println(productList.size());
+        Assert.assertTrue("The producted was not added", productList.size() == 1);
+    }
+
+    @Test
+    public void testAddProduct2() throws Exception {
+
+        //name is empty
+        Product p1=new Product(1,"","a",1);
+        ctrl.addProduct(p1);
+        List<Product> productList = ctrl.stockSituation();
+        System.out.println(productList.size());
+        Assert.assertTrue("The producted was not added", productList.size() == 1);
+    }
+
+
 }
